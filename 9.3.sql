4665274497f6f42a778a51939953192589b36303
DenisPt <67507367+DenisPt@users.noreply.github.com>
2020-10-16 21:07:44 +1000

    Create 9.3.sql

---
+++ 9.3.sql
@@ -0,0 +1,36 @@
+--------------------------------1--------------------------------
+delimiter //
+drop function if exists hello//
+create function hello()
+returns varchar(255) deterministic
+begin
+	declare h_now int default hour(now());
+	case
+		when h_now >= 6 and h_now < 12 then return 'Доброе утро!';
+		when h_now >= 12 and h_now < 18 then return 'Добрый день!';
+		when h_now >= 18 then return 'Добрый вечер!';
+		else return 'Доброй ночи!';
+	end case;
+end//
+
+select hello();
+
+--------------------------------2--------------------------------
+
+
+--------------------------------3--------------------------------
+drop function if exists fibonacci//
+create function fibonacci(num int)
+returns int deterministic
+begin
+	declare fib, fib_1, fib_2, i int default 0;
+	while (i <= num) do
+		set fib_2 = fib_1;
+		set fib_1 = fib;
+		set fib = fib_1 + fib_2;
+		if i = 1 then set fib = 1;
+		end if;
+		set i = i  +  1;
+	end while;
+	return fib;
+end//
