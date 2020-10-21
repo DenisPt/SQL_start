b9873b26d9f6c0078adaa13464722415ad94744a
DenisPt <67507367+DenisPt@users.noreply.github.com>
2020-10-21 11:29:43 +1000

    Update 11.1.2.sql

--- 11.1.2.sql
+++ 11.1.2.sql
@@ -6,8 +6,10 @@ drop procedure if exists create_users//
 create procedure create_users (in num int)
 begin
 	declare i int default 0;
+	declare usrs int;
+	select max(id) into usrs from users;
 	while i < num do
-		insert into users (name, birthday_at) values (concat('user', 1), date(from_unixtime(rand() * unix_timestamp(current_date)))); -- дата рождения рэндомная от 1970 до текущей даты
+		insert into users (name, birthday_at) values (concat('user', i + usrs), date(from_unixtime(rand() * unix_timestamp(current_date)))); -- дата рождения рэндомная от 1970 до текущей даты
 		set i = i + 1;
 	end while;
 end//
